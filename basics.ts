9a3b6292ea07285e55310b68885c9fec39990be8
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2021-05-17 07:57:04 +1000

    core types

---
+++ basics.ts
@@ -0,0 +1,18 @@
+const add = (n1: number, n2: number) => {
+  return n1 + n2;
+}
+
+// Type inference
+const number1 = 5
+const number2 = 3.6
+
+//Explicitly define the type - unneccesary
+const num3: number = 4
+
+
+// TS only helps before compililng to JS -> no browser support for TS
+const result = add(number1, number2)
+console.log(result)
+
+// typeof is a built in type checker from JS, however JS wont throw an error until runtime as oppose to 
+// TS that will throw it immediately because it checks during compilation.
